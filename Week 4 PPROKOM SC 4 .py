f001fc8929d00e09199452ca7ca07c12e2c4fd7f
wahyutrianto-19 <wahyutrianto@mail.ugm.ac.id>
2025-09-08 14:48:21 +0700

    Add files via upload

---
+++ Week 4 PPROKOM SC 4 .py
@@ -0,0 +1,15 @@
+# Nama file : nested_if_statement.py
+jenis_kelamin = "Pria"
+umur = 20
+if (jenis_kelamin == "Pria"):
+    if (umur >= 25):
+        print("Pria boleh Menikah")
+    else:
+        print("Pria tidak boleh Menikah")
+elif (Jenis_kelamin == "Wanita"):
+    if (umur >= 20):
+       print("Wanita boleh Menikah")
+    else:
+        print("Wanita tidak boleh Menikah")
+else:
+    print("Jenis kelamin tidak terdaftar")
