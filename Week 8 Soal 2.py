7ae07a4b1af82d30607b5b31d2017fa4814e5f7d
wahyutrianto-19 <wahyutrianto@mail.ugm.ac.id>
2025-10-20 21:05:05 +0700

    Add files via upload

---
+++ Week 8 Soal 2.py
@@ -0,0 +1,17 @@
+nama_teman = []
+for i in range(5):
+    nama = input(f"Masukkan nama teman ke-{i+1}: ")
+    nama_teman.append(nama)
+
+print("\nDaftar nama teman:")
+for i in range(len(nama_teman)):
+    print(f"Indeks {i}: {nama_teman[i]}")
+
+indeks = int(input("\nIngin mengganti nama pada indeks ke berapa? "))
+nama_baru = input("Masukkan nama penggantinya: ")
+
+nama_teman[indeks] = nama_baru
+
+print("\nDaftar nama teman setelah diperbarui:")
+for i in range(len(nama_teman)):
+    print(f"Indeks {i}: {nama_teman[i]}")
